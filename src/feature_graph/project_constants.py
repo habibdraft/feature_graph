96e492da5e4bfe0148935d4bf99cd52dded75703
Nazia.Habib <nazia.habib@patenergy.com>
2026-06-22 13:55:47 -0500

    removed notebooks

---
+++ src/feature_graph/project_constants.py
@@ -0,0 +1,22 @@
+baseline_df = 'TEP_FaultFree_Training.csv'
+df = 'TEP_Faulty_Training.csv'
+
+axes = {
+    "faultNumber": "fault_number",
+    "simulationRun": "simulation_run",
+    "sample": "sample",
+    "xmeas_7": "reactor_pressure",
+    "xmeas_8": "reactor_level",
+    "xmeas_9": "reactor_temperature",
+    "xmeas_11": "separator_temperature",
+    "xmeas_12": "separator_level",
+    "xmeas_13": "separator_pressure",
+    "xmeas_15": "stripper_level",
+    "xmeas_16": "stripper_pressure",
+    "xmeas_18": "stripper_temperature",
+    "xmeas_20": "compressor_work",
+    "xmeas_21": "reactor_cooling_water_outlet_temp",
+    "xmeas_22": "separator_cooling_water_outlet_temp",
+    "xmv_10": "reactor_cooling_water_valve",
+    "xmv_11": "condenser_cooling_water_valve",
+}
